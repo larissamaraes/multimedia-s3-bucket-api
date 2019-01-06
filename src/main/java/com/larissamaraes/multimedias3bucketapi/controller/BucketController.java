64a8ae8b90693f758d0ad27fe26536483e25d064
Larissa Marães <larissa.maraes@gmail.com>
2019-01-05 22:50:11 -0300

    creating controller

---
+++ src/main/java/com/larissamaraes/multimedias3bucketapi/controller/BucketController.java
@@ -0,0 +1,28 @@
+package com.larissamaraes.multimedias3bucketapi.controller;
+
+import com.larissamaraes.multimedias3bucketapi.service.BucketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage/")
+public class BucketController {
+
+    private BucketService bucketService;
+
+    @Autowired
+    BucketController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.bucketService.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.bucketService.deleteFileFromS3Bucket(fileUrl);
+    }
+}
