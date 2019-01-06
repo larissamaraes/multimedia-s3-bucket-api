0c52c39666dacf87c08bec4996b2a6bd45d4463c
Larissa Marães <larissa.maraes@gmail.com>
2019-01-05 22:23:31 -0300

    init project

---
+++ src/main/java/com/larissamaraes/multimedias3bucketapi/MultimediaS3BucketApiApplication.java
@@ -0,0 +1,14 @@
+package com.larissamaraes.multimedias3bucketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaS3BucketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimediaS3BucketApiApplication.class, args);
+	}
+
+}
+
